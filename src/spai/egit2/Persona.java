6269fc8f97429858cfaf654392fbebbaacfb9e73
monty-upm <todomontero@gmail.com>
2013-10-16 15:52:32 +0200

    estable

--- src/spai/egit2/Persona.java
+++ src/spai/egit2/Persona.java
@@ -9,6 +9,12 @@ public class Persona {
         cadena = cadena.trim();
         return cadena.substring(0, 1).toUpperCase() + cadena.substring(1).toLowerCase();
     }
+    
+    private String dni;
+    
+	public static final String[] LETRA_DNI = { "T", "R", "W", "A", "G", "M",
+		"Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H",
+		"L", "C", "K", "E" };
 
     public Persona(String nombre, String apellido) {
         this.setNombre(nombre);
@@ -30,6 +36,28 @@ public class Persona {
     public void setApellido(String apellido) {
         this.apellido = this.format(apellido);
     }
+    
+    public String getDni() {
+		return this.dni;
+	}
+
+	private void setDni(String dni) {
+		if(Persona.validaDni(dni)){
+			this.dni = dni;
+		}
+		else{
+			throw new IllegalArgumentException("Introduce un DNI válido con 8 numeros y una letra");
+		}
+	}
+	
+	public static boolean validaDni(String dni) {
+		String patron = "\\d{6,8}[a-zA-Z]";
+		if (!dni.matches(patron)) {
+			return false;
+		}
+		int numDni = Integer.parseInt(dni.substring(0, dni.length() - 1));
+		return (LETRA_DNI[(numDni % 23)].equals(dni.substring(dni.length() - 1, dni.length())));
+	}
 
     public String nombreCompleto() {
         return this.nombre + " " + this.apellido;
