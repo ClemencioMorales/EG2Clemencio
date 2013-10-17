7ec8c51555652aca16fdf55b5a7c30442b5b4481
manualvgar <manualvgar@gmail.com>
2013-10-17 17:05:18 +0200

    Corrección test NumeroEntero

--- test/spai/egit2/test/NumeroEnteroTest.java
+++ test/spai/egit2/test/NumeroEnteroTest.java
@@ -8,62 +8,63 @@ import org.junit.Test;
 import spai.egit2.NumeroEntero;
 
 public class NumeroEnteroTest {
-    private NumeroEntero numero;
+	private NumeroEntero numero;
 
-    @Before
-    public void initObjects() {
-        this.numero = new NumeroEntero(100);
-    }
+	@Before
+	public void initObjects() {
+		this.numero = new NumeroEntero(100);
+	}
 
-    @Test
-    public void testConstructorSinParametros() {
-        NumeroEntero numero = new NumeroEntero();
-        assertEquals(0, numero.getValor());
-    }
-    
-    @Test
-    public void testConstructorConParametros() {
-        NumeroEntero numero = new NumeroEntero(-10);
-        assertEquals(-10, numero.getValor());
-    }
+	@Test
+	public void testConstructorSinParametros() {
+		NumeroEntero numero = new NumeroEntero();
+		assertEquals(0, numero.getValor());
+	}
 
-    @Test
-    public void testIncrementar() {
-        this.numero.incrementar();
-        assertEquals(101, this.numero.getValor());
-    }
+	@Test
+	public void testConstructorConParametros() {
+		NumeroEntero numero = new NumeroEntero(-10);
+		assertEquals(-10, numero.getValor());
+	}
 
-    @Test
-    public void testDoblar() {
-        this.numero.doblar();
-        assertEquals(200, this.numero.getValor());
-    }
+	@Test
+	public void testIncrementar() {
+		this.numero.incrementar();
+		assertEquals(101, this.numero.getValor());
+	}
 
-    @Test
-    public void testDecrementar() {
-        this.numero.decrementar();
-        assertEquals(99, numero.getValor());
-        numero = new NumeroEntero();
-        this.numero.decrementar();
-        assertEquals(-1, numero.getValor());
-    }
-    
-    @Test
-    public void testIncrementarConValor(){
-    	this.numero.incrementar(15);
-    	assertEquals(25, this.numero.getValor());
-    	numero = new NumeroEntero(11);
-    	this.numero.incrementar(9);
-    	assertEquals(20, this.numero.getValor());
-    }
-    
-    @Test
-    public void testMultiplicarConValor(){
-    	this.numero.multiplicar(5);
-    	assertEquals(500, this.numero.getValor());
-    	numero = new NumeroEntero(11);
-    	this.numero.multiplicar(5);
-    	assertEquals(55, this.numero.getValor());
-    }
+	@Test
+	public void testDoblar() {
+		this.numero.doblar();
+		assertEquals(200, this.numero.getValor());
+	}
+
+	@Test
+	public void testDecrementar() {
+		this.numero.decrementar();
+		assertEquals(99, numero.getValor());
+		numero = new NumeroEntero();
+		this.numero.decrementar();
+		assertEquals(-1, numero.getValor());
+	}
+
+	@Test
+	public void testIncrementarConValor() {
+		this.numero = new NumeroEntero(10);
+		this.numero.incrementar(15);
+		assertEquals(25, this.numero.getValor());
+		numero = new NumeroEntero(11);
+		this.numero.incrementar(9);
+		assertEquals(20, this.numero.getValor());
+	}
+
+	@Test
+	public void testMultiplicarConValor() {
+		this.numero.multiplicar(5);
+		assertEquals(500, this.numero.getValor());
+		numero = new NumeroEntero(11);
+		this.numero.multiplicar(5);
+		assertEquals(55, this.numero.getValor());
+	}
 
 }
